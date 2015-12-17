2a8e4cf7b068e909a5d8135ecf42f123a6a3d8ab
Aleksi Peebles <aleksi@iki.fi>
2015-12-17 17:46:58 +0200

    Fixed bootstrap-code.sh to use new configuration variables

--- shila/provisioning/bootstrap-code.sh
+++ shila/provisioning/bootstrap-code.sh
@@ -10,5 +10,5 @@ mkdir -p ${CODE_DIR}
 
 # Shila Drupal
 cd ${CODE_DIR}
-(cd shila-drupal && git pull) || git clone -b ${SHILA_BRANCH} https://github.com/aleksip/shila-drupal
+(cd shila-drupal && git pull) || git clone -b ${SHILA_DRUPAL_BRANCH} ${SHILA_DRUPAL_REPO} shila-drupal
 ${CODE_DIR}/shila-drupal/scripts/bootstrap-code.sh
