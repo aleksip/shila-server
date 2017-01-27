bcd004745db461b54f03c9a8b07e8fe0a0a17929
Aleksi Peebles <aleksi@iki.fi>
2017-01-27 17:58:47 +0200

    Add Xdebug

--- shila/provisioning/bootstrap-host-vagrant.sh
+++ shila/provisioning/bootstrap-host-vagrant.sh
@@ -3,5 +3,21 @@
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${MY_DIR}/scripts.conf
 
+
+################################################################################
+# Install software
+################################################################################
+
+# Xdebug
+apt-get -y install php7.0-xdebug
+
+
+################################################################################
+# Configuration
+################################################################################
+
 # Nicer default shell for vagrant user
 chsh -s /bin/bash vagrant
+
+# Xdebug
+ln -fs ${CONF_ROOT}/etc/php/7.0/mods-available/xdebug.ini /etc/php/7.0/mods-available/xdebug.ini
