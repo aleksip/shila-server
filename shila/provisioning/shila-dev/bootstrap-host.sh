096d0b823a0256f9a5d0aeec6b054022c784eb43
Aleksi Peebles <aleksi@iki.fi>
2018-07-26 18:40:36 +0300

    Update PHP packages

--- shila/provisioning/shila-dev/bootstrap-host.sh
+++ shila/provisioning/shila-dev/bootstrap-host.sh
@@ -21,8 +21,8 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password password ${M
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${MYSQL_ROOT_PASSWORD}"
 apt-get -y install mysql-server mysql-client
 
-# PHP 7.0
-apt-get -y install php7.2-fpm php7.2-mysql php7.2-cli php7.2-gd php7.2-curl php7.2-mbstring php7.2-xml php7.2-zip php7.2-bcmath
+# PHP 7.2
+apt-get -y install php7.2-bcmath php7.2-bz2 php7.2-cli php7.2-curl php7.2-dev php7.2-fpm php7.2-gd php7.2-geoip php7.2-imagick php7.2-intl php7.2-mbstring php7.2-mysql php7.2-xml php7.2-zip
 
 # Nginx
 apt-get -y install nginx
